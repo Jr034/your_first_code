945240ed8973fe08db0648c542db5458816146a2
Jr034 <67443634+Jr034@users.noreply.github.com>
2020-06-25 18:17:06 -0500

    I Hope be Doing it!
    Sorry, I don`t Know any way for do it!.. for  now of course!!..   Thanks for Watch!!  (_8^(|)

--- main.c
+++ main.c
@@ -7,3 +7,17 @@ int main()
   }
   return 0;
 }
+
+int main()
+{
+  int i;
+  for (i = 0; i < 32; i++)
+  {
+    printf("Hello Holberton\n");
+  }
+  return 0;
+}
+
+
+git branch develop
+git push -u origin develop
